7277bb66b66013654fcfa5d57976444568f1e880
Zayed Ahmed <mailsajjad006@gmail.com>
2020-12-02 00:26:42 +0530

    added subtotal component

---
+++ src/Subtotal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Subtotal.css";
+
+function Subtotal() {
+  return (
+    <div className="subtotal">
+      <h1>this is subtotal</h1>
+    </div>
+  );
+}
+
+export default Subtotal;
